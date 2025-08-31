6a1da034a9143f2eb39b99d5c39e898bd77159ff
Just-Prog <JustProg5835@outlook.com>
2025-08-31 08:42:12 +0800

    chore: `vercel.json`

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverComponentsExternalPackages: ['NeteaseCloudMusicApi'],
-  },
+  serverExternalPackages: ['NeteaseCloudMusicApi'],
 };
 
 export default nextConfig;
--- vercel.sh
+++ vercel.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sed -i '' -e "s/git@github.com\//$GITHUB_TOKEN@github.com\//g" package.json
+sed -i -e "s/git+https:\/\/github.com\/Just-Prog/git+https:\/\/Just-Prog:$GITHUB_TOKEN@github.com\/Just-Prog/g" package.json
 echo "-⭐- 已替换 private 库源"
-pnpm install
+pnpm install --no-frozen-lockfile
 echo "-👍- 安装完毕"
+pnpm build

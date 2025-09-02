930bce94e804cbc4a1cc27f3f18930c2050af7c4
Just-Prog <JustProg5835@outlook.com>
2025-09-02 11:41:31 +0800

    chore: `vercel.json` & `vercel.sh`

---
+++ vercel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sed -i -e "s/git+https:\/\/github.com\/Just-Prog/git+https:\/\/Just-Prog:$GITHUB_TOKEN@github.com\/Just-Prog/g" package.json
+echo "-⭐- 已替换 private 库源"
+pnpm install --no-frozen-lockfile
+echo "-👍- 安装完毕"
+#pnpm build
+#echo "-👍- 构建完毕"

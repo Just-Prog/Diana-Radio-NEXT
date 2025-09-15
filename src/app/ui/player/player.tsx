1108eb2148a1d20c5ee8e732450f0419af51cc41
Just-Prog <JustProg5835@outlook.com>
2025-09-15 09:30:09 +0800

    feat: 播放器底栏样式构建
    TODO: 实现实际功能

--- src/app/ui/player/player.tsx
+++ src/app/ui/player/player.tsx
@@ -1,5 +1,14 @@
+'use client';
+import { Button } from '@arco-design/web-react';
+import {
+  IconBackward,
+  IconForward,
+  IconPlayArrow,
+  IconSkipNext,
+  IconSkipPrevious,
+  IconSound,
+} from '@arco-design/web-react/icon';
 import Image from 'next/image';
-
 import jianwen_cover from '@/app/assets/program/jianwen.png';
 import songs_cover from '@/app/assets/program/songs.png';
 import sleep_cover from '@/app/assets/program/songs.png';
@@ -10,6 +19,14 @@ const programCover = {
   sleep: sleep_cover,
 };
 
+const PlayerControllerButton = ({ children }) => {
+  return (
+    <span className="cursor-pointer rounded-2xl px-4 pt-2 pb-1 hover:bg-gray-400/20">
+      {children}
+    </span>
+  );
+};
+
 const Player: React.FC<{
   type: 'songs' | 'sleep' | 'jianwen';
   songInfo: any;
@@ -18,20 +35,40 @@ const Player: React.FC<{
     <div className="flex h-full w-full flex-col">
       <div className="flex-1">
         <div className="flex h-full select-none flex-col items-center justify-center gap-y-8">
-          <Image
-            alt="cover"
-            className={
-              'w-40 animate-[spin_3s_linear_infinite] rounded-[50%] shadow-black shadow-xl/10 md:w-50'
-            }
-            src={programCover[type]}
-          />
+          <div className="overflow-clip rounded-[50%] shadow-black shadow-xl/10">
+            <Image
+              alt="cover"
+              className={'w-40 animate-[spin_3s_linear_infinite] md:w-50'}
+              src={programCover[type]}
+            />
+          </div>
+
           <span className="font-bold text-black/85 text-xl md:text-2xl">
             SongName
           </span>
         </div>
       </div>
       <div className="flex h-16 w-full items-center justify-center bg-white/60 backdrop-blur-lg">
-        ControllerBar
+        <div className="flex gap-x-4">
+          <PlayerControllerButton>
+            <IconSkipPrevious className="text-lg text-white" />
+          </PlayerControllerButton>
+          <PlayerControllerButton>
+            <IconBackward className="text-lg text-white" />
+          </PlayerControllerButton>
+          <PlayerControllerButton>
+            <IconPlayArrow className="text-lg text-white" />
+          </PlayerControllerButton>
+          <PlayerControllerButton>
+            <IconForward className="text-lg text-white" />
+          </PlayerControllerButton>
+          <PlayerControllerButton>
+            <IconSkipNext className="text-lg text-white" />
+          </PlayerControllerButton>
+          <PlayerControllerButton>
+            <IconSound className="text-lg text-white" />
+          </PlayerControllerButton>
+        </div>
       </div>
     </div>
   );

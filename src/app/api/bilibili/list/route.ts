d6b84e2d65ca20201d87194de139615988a308b0
Just-Prog <JustProg5835@outlook.com>
2025-10-23 09:27:15 +0800

    feat: 支持代理检索bilibili音乐区视频和检索翻页

---
+++ src/app/api/bilibili/list/route.ts
@@ -0,0 +1,40 @@
+import type { NextRequest } from 'next/server';
+import WbiSigner from '@/app/lib/api/utils/wbi_sign';
+import Request from '@/app/lib/axios/request';
+import { BilibiliHeaders } from '../../podcast/constants';
+
+export async function GET(
+  req: NextRequest,
+  context: RouteContext<'/api/bilibili/list'>
+) {
+  try {
+    const pageNum = req.nextUrl.searchParams.get('page') ?? 1;
+    const target = 'https://api.bilibili.com/x/web-interface/search/all/v2';
+    const params = await WbiSigner({
+      page: pageNum,
+      page_size: 30,
+      tids: 3,
+      device: 'win',
+      mobi_app: 'pc_electron',
+      keyword: '嘉然',
+      web_location: 'bilibili-electron',
+    });
+    const data = (
+      await Request.get(target, { params, headers: BilibiliHeaders })
+    ).data.data.result.find((v) => v.result_type === 'video').data;
+    return Response.json({
+      code: 200,
+      data,
+    });
+  } catch (e) {
+    return Response.json(
+      {
+        code: 500,
+        msg: e.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
--- src/app/api/podcast/constants.ts
+++ src/app/api/podcast/constants.ts
@@ -22,6 +22,24 @@ const DianaWeeklyAvailableProgramsInfo = [
   { name: '嘉然的哈基米音乐', key: 'hachimi', cover: hachimi_cover },
 ];
 
+const BilibiliCookies = {
+  sessdata: '',
+};
+
+const BilibiliHeaders = {
+  accept: '*/*',
+  'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+  origin: 'https://www.bilibili.com',
+  referer: 'https://www.bilibili.com/client',
+  'sec-ch-ua':
+    '"Google Chrome";v="141", "Not?A_Brand";v="8", "Chromium";v="141"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': 'windows',
+  cookie: 'SESSDATA=;',
+  'user-agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36 Edg/141.5.1.4',
+};
+
 export {
   PC_SONGS,
   PC_SLEEPSTORY,
@@ -33,4 +51,6 @@ export {
   sleep_cover,
   DianaWeeklyAvailablePodcasts,
   DianaWeeklyAvailableProgramsInfo,
+  BilibiliHeaders,
+  BilibiliCookies,
 };

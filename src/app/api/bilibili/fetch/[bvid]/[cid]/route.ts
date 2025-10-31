229c1e5f553d66a2b41128babfeeef8897997073
Just-Prog <JustProg5835@outlook.com>
2025-10-31 16:53:39 +0800

    feat: 修改哔哩哔哩音频返回格式，替换pcdn域名

--- src/app/api/bilibili/fetch/[bvid]/[cid]/route.ts
+++ src/app/api/bilibili/fetch/[bvid]/[cid]/route.ts
@@ -3,6 +3,9 @@ import { BilibiliHeaders } from '@/app/api/podcast/constants';
 import WbiSigner from '@/app/lib/api/utils/wbi_sign';
 import Request from '@/app/lib/axios/request';
 
+const pcdn_pattern =
+  /https:\/\/xy\d+x\d+x\d+x\d+xy\.mcdn\.bilivideo\.cn(:\d+)?/g;
+
 export async function GET(
   req: NextRequest,
   ctx: RouteContext<'/api/bilibili/fetch/[bvid]/[cid]'>
@@ -24,9 +27,20 @@ export async function GET(
     }
     return cur;
   });
-  const { base_url, backup_url, bandwidth, mime_type, codecs } = audio;
+  let { base_url, backup_url, bandwidth, mime_type, codecs } = audio;
+  base_url = base_url.replace(
+    pcdn_pattern,
+    'https://upos-sz-mirrorcos.bilivideo.com'
+  );
   return Response.json({
     code: 0,
-    data: { base_url, backup_url, bandwidth, mime_type, codecs },
+    data: {
+      base_url,
+      backup_url,
+      bandwidth,
+      mime_type,
+      codecs,
+      referer: `https://www.bilibili.com/${bvid}`,
+    },
   });
 }

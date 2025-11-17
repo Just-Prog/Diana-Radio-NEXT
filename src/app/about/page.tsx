cd62d3b49985fd220b73f63872326f68bdf7c416
Just-Prog <JustProg5835@outlook.com>
2025-11-17 17:19:52 +0800

    feat: 关于页修缮

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,12 @@
+"use client";
+import { IconGithub, IconVideoCamera } from "@arco-design/web-react/icon";
 import Image from "next/image";
 import logo from "@/app/assets/logo_horizon.png";
 import Divider from "../ui/common/divider";
 
 const AboutPage = () => (
   <div className="flex flex-col justify-center px-8 py-4 md:flex-row">
-    <div className="">
+    <div className="flex items-center justify-center">
       <Image
         alt="logo"
         className="h-full w-3xs self-center object-contain"
@@ -15,7 +17,29 @@ const AboutPage = () => (
     <div className="flex flex-col justify-center text-center md:py-8 md:text-left">
       <div className="font-bold text-3xl">Diana Radio 嘉然电台</div>
       <div className="font-normal text-lg">不只是为 NCM 适配的替代前端页面</div>
-      <div className="font-normal text-lg">@嘉心糖周报 @Just-Prog</div>
+      <div className="flex flex-row items-center gap-x-1 font-normal text-lg">
+        <IconVideoCamera className="text-[#e799b0]!" />
+        <span
+          className="mr-2"
+          onClick={() => {
+            if (typeof window !== "undefined") {
+              window.open("https://space.bilibili.com/247210788", "_blank");
+            }
+          }}
+        >
+          @嘉心糖周报
+        </span>
+        <IconGithub />
+        <span
+          onClick={() => {
+            if (typeof window !== "undefined") {
+              window.open("https://github.com/Just-Prog", "_blank");
+            }
+          }}
+        >
+          @Just-Prog
+        </span>
+      </div>
     </div>
   </div>
 );
